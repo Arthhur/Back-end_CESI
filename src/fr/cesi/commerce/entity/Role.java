9926495eb4a728802be7a6b5540953aec8a2343b
bpaumard <benjamin53200@gmail.com>
2019-11-21 11:08:44 +0100

    feat(role)

--- src/fr/cesi/commerce/entity/Role.java
+++ src/fr/cesi/commerce/entity/Role.java
@@ -18,23 +18,9 @@ public class Role {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id  ;
+	private String libelle ;
 	
-	@ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "role_id", referencedColumnName = "id")
-	private Role role;
-	
-	@ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "service_id", referencedColumnName = "id")
-	private Service service;
-	
-    private String nom = "" ;
-    private String prenom = "" ;
-    private String sexe = "" ;
-    private String date_naissance ;
-    private String email = "" ;
-    private String pass = "" ;
-	
-	public role() {	
+	public Role() {	
 	}
 
 	public Long getId() {
@@ -45,68 +31,12 @@ public class Role {
 		this.id = id;
 	}
 
-	public Role getRole() {
-		return role;
+	public String getLibelle() {
+		return libelle;
 	}
 
-	public void setRole(Role role) {
-		this.role = role;
-	}
-
-	public Service getService() {
-		return service;
-	}
-
-	public void setService(Service service) {
-		this.service = service;
-	}
-
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public String getPrenom() {
-		return prenom;
-	}
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-	public String getSexe() {
-		return sexe;
-	}
-
-	public void setSexe(String sexe) {
-		this.sexe = sexe;
-	}
-
-	public String getDate_naissance() {
-		return date_naissance;
-	}
-
-	public void setDate_naissance(String date_naissance) {
-		this.date_naissance = date_naissance;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPass() {
-		return pass;
-	}
-
-	public void setPass(String pass) {
-		this.pass = pass;
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
 	}
 
 	
