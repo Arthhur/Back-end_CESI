bf39e63838c7eff284a5ac007dc9195e3d1e04d6
bpaumard <benjamin53200@gmail.com>
2019-11-21 16:40:00 +0100

    confict

--- src/fr/cesi/commerce/entity/Collaborateur.java
+++ src/fr/cesi/commerce/entity/Collaborateur.java
@@ -18,7 +18,7 @@ public class Collaborateur {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id  ;
 	
-	@OneToMany(fetch = FetchType.EAGER)
+	@ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "id_role", referencedColumnName = "id")
 	private Role role ;
 	
